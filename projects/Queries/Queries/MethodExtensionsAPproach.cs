1cc7cc194ec3d3bee9ee99b8121a60f24d6c47a8
Nikita Starostin <n.starostin@itransition.com>
2017-07-27 22:18:03 +0300

    method extensions approach

---
+++ projects/Queries/Queries/MethodExtensionsAPproach.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class MethodExtensionsApproach : IApproach
+    {
+        public void Main()
+        {
+            var context = new PlutoContext();
+
+            //var courses = context.Courses.Where(c => c.Name.Contains("c#"));
+
+
+            //var courses = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name).ThenBy(c => c.Level);
+            //var courses = context.Courses.Where(c => c.Name.Contains("c#")).OrderByDescending(c => c.Name).ThenByDescending(c => c.Level);
+            //var courses = context.Courses
+            //    .Where(c => c.Name.Contains("c#"))
+            //    .OrderByDescending(c => c.Name)
+            //    .ThenByDescending(c => c.Level)
+            //    .Select(c => new { CourseName = c.Name, AuthorName = c.Author.Name });
+            //var courses = context.Courses
+            //    .Where(c => c.Name.Contains("c#"))
+            //    .OrderByDescending(c => c.Name)
+            //    .ThenByDescending(c => c.Level)
+            //    .Select(c => c.Tags);
+
+            //foreach ( var c in courses)
+            //{
+            //    foreach(var tag in c)
+            //    {
+            //        Console.WriteLine(tag.Name);
+            //    }
+            //}
+            //var tags = context.Courses
+            //    .Where(c => c.Name.Contains("c#"))
+            //    .OrderByDescending(c => c.Name)
+            //    .ThenByDescending(c => c.Level)
+            //    .SelectMany(c => c.Tags)
+            //    .Distinct();
+
+            //foreach (var tag in tags)
+            //{
+            //    Console.WriteLine(tag.Name);
+            //}
+            //var groups = context.Courses.GroupBy(c => c.Level);
+            //foreach (var group in groups)
+            //{
+            //    Console.WriteLine("Key: " + group.Key);
+            //    foreach (var course in group)
+            //    {
+            //        Console.WriteLine("\t" + course.Name);
+            //    }
+            //}
+
+            //context.Courses.Join(context.Authors,
+            //    c => c.AuthorId,
+            //    a => a.Id,
+            //    (course, author) => new
+            //    {
+            //        CourseName = course.Name,
+            //        AuthorName = author.Name
+            //    });
+
+            //context.Authors.GroupJoin(context.Courses,
+            //    a => a.Id,
+            //    c => c.Id,
+            //    (author, courses) =>
+            //    new {
+            //        AuthorName = author.Name,
+            //        AmountOfCourses = courses.Count()
+            //    });
+
+            //context.Authors.SelectMany(a => context.Courses, (author, course) =>
+            //new {
+            //    AuthorName = author.Name,
+            //    CourseName = course.Name
+            //});
+        }
+    }
+}
---
+++ projects/Queries/Queries/MethodExtensionsApproachAdditional.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class MethodExtensionsApproachAdditional : IApproach
+    {
+        public void Main()
+        {
+            var context = new PlutoContext();
+
+            //var page2 = context.Courses.Skip(10).Take(10);
+
+            //context.Courses.OrderBy(c => c.Level).FirstOrDefault(c => c.FullPrice > 100); //or First()
+            //context.Courses.SingleOrDefault(c => c.Id == 1);
+
+            //var allAbove10 = context.Courses.All(c => c.FullPrice > 10);
+            //context.Courses.Any(c => c.Level == 1);
+
+            //var count = context.Courses.Count();
+            //var mostExpensive = context.Courses.Max(c => c.FullPrice);
+            //var leastExpensive = context.Courses.Min(c => c.FullPrice);
+            //var avgPrice = context.Courses.Average(c => c.FullPrice);
+            //var mostExpensiveFirstLevel = context.Courses.Where(c => c.Level == 1).Max(c => c.FullPrice);
+        }
+    }
+}
