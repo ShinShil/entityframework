733da2d3ad2e7ff3757dca7a72e6f73369ad25c9
Nikita Starostin <n.starostin@itransition.com>
2017-07-27 11:10:54 +0300

    learning datafirst

---
+++ projects/DbFirst/DbFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlutoDbContext plutoDb = new PlutoDbContext();
+            var courses = plutoDb.GetCourses();
+ 
+            foreach(var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+        }
+    }
+}
