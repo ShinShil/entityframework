af8fc207a292e1d431de138dccd149476b7550c6
Nikita Starostin <n.starostin@itransition.com>
2017-07-27 21:43:07 +0300

    linq approach

--- projects/Queries/Queries/Program.cs
+++ projects/Queries/Queries/Program.cs
@@ -9,26 +9,50 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            // LINQ syntax
+            //var query =
+            //    from c in context.Courses
+            //    where c.Author.Id == 1
+            //    orderby c.Level descending, c.Name
+            //    select new { Name = c.Name };
+            ///////////////////////////////////////////////////////
+            //var query =
+            //    from c in context.Courses
+            //    group c by c.Level
+            //    into g
+            //    select g;
+            //
+            //foreach(var group in query)
+            //{
+            //    Console.WriteLine("{0} ({1})", group.Key, group.Count());
+            //    //foreach (var course in group)
+            //    //{
+            //    //    Console.WriteLine("\t{0}", course.Name);
+            //    //}
+            //}
+            ////////////////////////////////////////////////////////
+            //var query =
+            //    from c in context.Courses
+            //    join a in context.Authors on c.AuthorId equals a.Id
+            //    select new { CourseName = c.Name, AuthorName = a.Name };
+            ////////////////////////////////////////////////////////
+            //var query =
+            //    from a in context.Authors
+            //    join c in context.Courses on a.Id equals c.AuthorId into g
+            //    select new { AuthorName = a.Name, Courses = g.Count() };
+
+            //foreach(var x in query)
+            //{
+            //    Console.WriteLine("{0} ({1})", x.AuthorName, x.Courses);
+            //}
+            ////////////////////////////////////////////////////////
             var query =
                 from c in context.Courses
-                where c.Name.Contains("c#")
-                orderby c.Name
-                select c;
+                from a in context.Authors
+                select new { AuthorName = a.Name, CourseName = c.Name };
 
-            foreach(var course in query)
+            foreach(var x in query)
             {
-                Console.WriteLine(course.Name);
-            }
-
-            // Extensions methods
-            var courses = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .OrderBy(c => c.Name);
-
-            foreach (var course in query)
-            {
-                Console.WriteLine(course.Name);
+                Console.WriteLine("{0} - {1}", x.AuthorName, x.CourseName);
             }
         }
     }
