728ed3095a0a5f488b007343f2dddf44bf6b6727
Nikita Starostin <n.starostin@itransition.com>
2017-07-27 20:18:16 +0300

    Classfication column in code first exercise

--- projects/CodeFirstExercise/CodeFirstExercise/Video.cs
+++ projects/CodeFirstExercise/CodeFirstExercise/Video.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace CodeFirstExercise
 {
+    public enum VideoClassification
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
+    }
     public class Video
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public VideoClassification Classification { get; set; }
         public virtual Genre Genre { get; set; }
     }
 }
